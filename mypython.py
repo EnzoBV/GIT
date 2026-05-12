0c4e084ae4b635afbc00d7a510387f9d39477261
zomel <enzomelgarejobv@gmail.com>
2026-05-12 14:49:44 -0300

    subi ficheros modificados

---
+++ mypython.py
@@ -0,0 +1,4 @@
+print("Hello Git!")
+print("¡Como estuvo el curso?")
+print ("Estoy editando el fichero")
+
---
+++ mypython2.py
@@ -0,0 +1,3 @@
+print ("Este es el segundo fichero que creo para git")
+print ("Oh yes")
+print ("Estoy modificando el fichero 2 de python")
